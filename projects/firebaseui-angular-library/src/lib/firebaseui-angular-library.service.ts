5181df28a77f178f58e8d523fea6d3f2ef450ba6
omol-crystalogic <58267082+omol-crystalogic@users.noreply.github.com>
2019-12-02 20:06:33 +0200

    Update firebaseui-angular-library.service.ts

--- projects/firebaseui-angular-library/src/lib/firebaseui-angular-library.service.ts
+++ projects/firebaseui-angular-library/src/lib/firebaseui-angular-library.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
-import * as firebaseui from 'firebaseui';
+
 
 @Injectable()
 export class FirebaseuiAngularLibraryService {
